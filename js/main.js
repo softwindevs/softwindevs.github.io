fa989bb500fc205a21c982eabf8f4fced5d74cf0
SOFTWIN Developer's <49920353+softwindevs@users.noreply.github.com>
2019-05-07 13:14:00 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "https://formspree.io/clasherhacksu@gmail.com",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
